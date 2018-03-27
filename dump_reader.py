6952bd0a09c528c109ddc43ed5fc586a57c3ff80
oya10 <32030081+oya10@users.noreply.github.com>
2018-03-27 13:41:31 +0800

    Add files via upload

---
+++ dump_reader.py
@@ -0,0 +1,78 @@
+import os,re,sys
+import datetime
+import csv
+
+def try_dump(): ##returns boolean
+    if not os.path.isdir('01 results'):
+##        print ('can\'t find ma results')
+        return False
+    else:
+        try:
+            file = open('01 results/000 dump.txt','r')
+            return True
+        except FileNotFoundError:
+            print ('dump not found. this must be your first time.')
+    
+def read_dump(): ##returns 2-element tuple. [0] is address, [1] is activity record
+    print('found dump. reading 000 dump.txt ...')
+
+    file = open('01 results/000 dump.txt','r',encoding='latin-1')
+    curr_line = file.readline()
+
+    client_ip_record = {}
+    header = ''
+
+    counter = 1
+    while curr_line != '': ## and counter < 10:
+        counter += 1
+        header_pattern = ('##\s\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s\'s\sdump\s##')
+        result = ''
+        if re.search(header_pattern,curr_line):
+            result = re.search('##\s(.+?)\s\'s\sdump\s##',curr_line)
+            header = result.group(1)
+            counter += 1
+            client_ip_record[header] = []
+            
+        if header != '' and curr_line != '\n':
+            if not re.search(header_pattern,curr_line):
+                ##tupled = curr_line
+                client_ip_record[header].append(eval(curr_line))
+                
+        curr_line = file.readline()
+    unique_client_ip_set = set(client_ip_record.keys())
+    print('done')
+    return (unique_client_ip_set,client_ip_record)
+
+##csv module
+def dump_csv():
+    filename = '01 results/000csv dump.csv'
+    print ('writing 000csv dump.csv ...')
+
+    to_dump = read_dump()[1]
+    
+    try:
+        csvfile = open(filename,'r')
+    except FileNotFoundError:
+        csvfile = open(filename,'w+')
+            
+    with open(filename,'w',newline='',encoding='latin-1') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['ip address','activity','year','month','day','hour','minute','second','port','user-agent','http status code'])
+        for key in to_dump:
+            for line in to_dump[key]:
+                csvrow = []
+                for i in line:
+                    if type(i) is datetime.datetime:
+                        csvrow = csvrow + [i.year] + [i.month] + [i.day] + [i.hour] + [i.minute] + [i.second]
+                    else:
+                        csvrow.append(i)
+##                print([key]+csvrow)
+                csvwriter.writerow([key]+csvrow)
+    print('csv done.')
+                
+
+###########
+##dump_csv()
+##read_dump()
+##try_dump()
+###########
---
+++ main.py
@@ -0,0 +1,292 @@
+import os,re,sys
+import datetime
+import analyze_query as aq
+import dump_reader
+
+###try creating result files
+##test = os.getcwd()
+###print (test)
+##if not os.path.isdir('result'):
+##    os.makedirs('result')
+
+def define_variables():
+    p = {
+		's' : r'\s',					# space
+		'0' : r'\d{4}-\d{2}-\d{2}',			# date
+		'1' : r'\d{2}:\d{2}:\d{2}',			# time
+		'2' : r'172.17.100.\d{1,3}',			# server_ip
+		'3' : 'GET|POST|HEAD|OPTIONS',			# method
+		'4' : '.*',					# uri_stem & query
+		'5' : '80|443',					# port
+		'6' : '.*',					# client_username
+		'7' : r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',	# client_ip
+		'8' : '.*',					# client_agent
+		'9' : r'\d{3}',					# status
+		'10' : r'\d{1,3}',				# substatus
+		'11' : r'\d{1,3}',				# win_status
+		'12' : r'\d{1,3}'                               # time_taken
+        }
+
+    pattern = ""
+    for i in range(len(p)-2):
+            pattern = pattern+"("+p[str(i)]+")"
+            if i < len(p)-3 and i != 4:
+                    pattern = pattern + p['s']
+                    
+    filename = input('please enter filename of apache log:\n')
+    try:
+        file = open(os.getcwd()+'/'+filename,'r')
+    except FileNotFoundError:
+        print('log doesn\'t exist or wrong filename. aborting.')
+        sys.exit()
+        
+    extract_data(pattern,filename)
+
+def map(op,seq): #emergency map func cos I'm unfamiliar with python's native map func
+    if seq == []:
+        return seq
+    else:
+        return [op(seq[0])] + map(op,seq[1:])
+
+def extract_data(pattern,filename):
+    print('extracting data from '+ filename + ' ...')
+    file = open(os.getcwd()+'/'+filename,'r',encoding='latin-1')
+    client_ip_record = {}
+    unique_client_ip_set = set()
+
+    curr_line = file.readline()
+    while curr_line != '':
+        result = re.match(pattern,curr_line)
+        if result is None:
+            pass
+        ##exclude css, images, javascript, jqueries, tooltips, popups
+        elif any(re.findall(r'/images/|.jpg|.gif|.ico|/tooltip|popup|.css|/css|/theme/|/jquery',result.groups(0)[4],re.IGNORECASE)): 
+            curr_line = file.readline()
+            continue
+        else:
+            ddate,ttime = result.groups(0)[0].split('-'),result.groups(0)[1].split(':') #date-time
+            ddate,ttime = map(lambda x:int(x),ddate),map(lambda x:int(x),ttime) #convert to integer
+            timestamp = datetime.datetime(ddate[0],ddate[1],ddate[2],ttime[0],ttime[1],ttime[2])
+
+            activity = result.groups(0)[3]+' '+result.groups(0)[4] #query
+            client_ip = result.groups(0)[7] #ip address
+            client_port = result.groups(0)[5]#port
+            client_agent = result.groups(0)[8]#user-agent
+            client_success = result.groups(0)[9] #status code
+            
+            unique_client_ip_set.add(client_ip)
+            organize_record(result,timestamp,activity,client_ip,client_port,client_agent,client_success,client_ip_record)
+
+        curr_line = file.readline()
+    print('done')
+    print_results(unique_client_ip_set,client_ip_record)
+    
+def organize_record(result,timestamp,activity,client_ip,client_port,client_agent,client_success,client_ip_record):
+    if client_ip not in client_ip_record.keys():
+            client_ip_record[client_ip] = [1,(activity,timestamp,client_port,client_agent,client_success)]
+    else:
+            client_ip_record[client_ip][0] = client_ip_record[client_ip][0] + 1
+            ##create list of tuples for each activity
+            client_ip_record[client_ip].append((activity,timestamp,client_port,client_agent,client_success))      
+    return client_ip_record
+
+#each client's record (in one tuple):
+#[0] activity
+#[1] timestamp
+#[2] port
+#[3] user-agent
+#[4] http status code
+
+##printing results (includes evaluation of server log)
+def print_results(unique_client_ip_set,client_ip_record):
+    print('ensuring results folder exists ... /01 results')
+    if not os.path.isdir('01 results'):
+        os.makedirs('01 results')
+        
+#000 dump ##may or may not be useful for re-analysis
+    '''
+    - subsequent scans can be done after tweaking the analyze_query module.
+    - reading from 000 dump.txt about halves the time from extracting data
+      from CTF1.log
+    '''
+    filename = '000 dump.txt'
+    print('0/6 writing 000 dump.txt ...')
+    try:
+        file = open('01 results/'+filename,'r')
+    except FileNotFoundError:
+        file = open('01 results/'+filename,'w+')
+
+    with open(os.getcwd()+'/01 results/'+filename,'w',encoding='utf-8',errors='ignore') as file:
+        for client in unique_client_ip_set:
+            file.write('\n\n## '+client+' \'s dump ##\n')
+            for record in client_ip_record[client][1:]:
+                file.write(str(record)+'\n')
+    
+#001 ip_addresses (may or may not be helpful)
+    filename = '001 ip_address_list.txt'
+    print('1/6 writing ' + filename + ' ...')
+    try:
+        file = open('01 results/'+filename,'r')
+    except FileNotFoundError:
+        file = open('01 results/'+filename,'w+')
+        
+    with open(os.getcwd()+'\\01 results\\'+filename,'w',encoding='utf-8',errors='ignore') as file:
+        file.write('#############################\n')
+        file.write('## All unique ip addresses ##\n')
+        file.write('#############################\n\n')
+        for client in unique_client_ip_set:
+            file.write(client+'\n')
+
+#002 ip_address_activities (most helpful for manual eye-scan)
+    filename = '002 ip_address_activities.txt'
+    print('2/6 writing ' + filename + ' ...')    
+    try:
+        file = open('01 results/'+filename,'r')
+    except FileNotFoundError:
+        file = open('01 results/'+filename,'w+')
+
+    with open(os.getcwd()+'/01 results/'+filename,'w',encoding='utf-8',errors='ignore') as file:
+        file.write('##########################################\n')
+        file.write('## All activities per unique ip address ##\n')
+        file.write('##########################################\n\n')
+        for client in unique_client_ip_set:
+            file.write('## '+client+' \'s activities ##\n')
+            for record in client_ip_record[client][1:]:
+                file.write(record[1].strftime('%Y-%m-%d %H:%M:%S')+' '+record[2]+' '+record[4]+' '+record[0].strip(' - ')+'\n')
+            file.write('\n')
+    
+#003 sql_injections
+    filename = '003 sql_injections.txt'
+    print('3/6 writing/evaluating ' + filename + ' ...')
+    try:
+        file = open('01 results/'+filename,'r')
+    except FileNotFoundError:
+        file = open('01 results/'+filename,'w+')        
+
+    with open(os.getcwd()+'/01 results/'+filename,'w',encoding='utf-8',errors='ignore') as file:
+        file.write('###################################################\n')
+        file.write('## Probable SQLi attempts (some false positives) ##\n')
+        file.write('###################################################\n\n')
+        for client in unique_client_ip_set:
+            next_line = False
+            to_write = []
+            for record in client_ip_record[client][1:]:
+                if aq.detect_sqli(record[0]):
+                    #try counter for each client_ip
+                    next_line = True
+                    to_write.append(str((record[1].strftime('%Y-%m-%d %H:%M:%S')+' '+record[0].strip(' - '))))
+            if next_line:
+                file.write('## '+client+' \'s sqli attempts ##\n')
+                for record in to_write:
+                    file.write(record+'\n')
+                file.write('\n')
+    
+#004 file_inclusions
+    filename = '004 file_inclusions.txt'
+    print('4/6 writing/evaluating ' + filename + ' ...')
+    try:
+        file = open('01 results/'+filename,'r')
+    except FileNotFoundError:
+        file = open('01 results/'+filename,'w+')
+
+    with open(os.getcwd()+'/01 results/'+filename,'w',encoding='utf-8',errors='ignore') as file:
+        file.write('##########################################################################\n')
+        file.write('## Probable remote/local file inclusion attempts (some false positives) ##\n')
+        file.write('##########################################################################\n\n')
+        for client in unique_client_ip_set:
+            next_line = False
+            to_write = []
+            for record in client_ip_record[client][1:]:
+                if aq.detect_rfi(record[0]):
+                    next_line = True
+                    to_write.append(str((record[1].strftime('%Y-%m-%d %H:%M:%S')+' '+record[0].strip(' - '))))
+            if next_line:
+                file.write('## '+client+' \'s rfi/lfi attempts ##\n')
+                for record in to_write:
+                    file.write(record+'\n')
+                file.write('\n')
+    
+#005 web_shells
+    filename = '005 web_shells.txt'
+    print('5/6 writing/evaluating ' + filename + ' ...')
+    try:
+        file = open('01 results/'+filename,'r')
+    except FileNotFoundError:
+        file = open('01 results/'+filename,'w+')
+
+    with open(os.getcwd()+'/01 results/'+filename,'w',encoding='utf-8',errors='ignore') as file:
+        file.write('########################################################\n')
+        file.write('## Probable web shell attempts (some false positives) ##\n')
+        file.write('########################################################\n\n')
+        for client in unique_client_ip_set:
+            next_line = False
+            to_write = []
+            for record in client_ip_record[client][1:]:
+                if aq.detect_web_shell(record[0]):
+                    next_line = True
+                    to_write.append(str((record[1].strftime('%Y-%m-%d %H:%M:%S')+' '+record[0].strip(' - '))))
+            if next_line:
+                file.write('## '+client+' \'s web-shell attempts ##\n')
+                for record in to_write:
+                    file.write(record+'\n')
+                file.write('\n')
+
+#006 combined_sqli_rfi_shells (for high-level overview of suspicious activity)
+    filename = '006 combined_sqli_rfi_shells.txt'
+    print('6/6 writing/evaluating ' + filename + ' ...')
+    try:
+        file = open('01 results/'+filename,'r')
+    except FileNotFoundError:
+        file = open('01 results/'+filename,'w+')
+
+    with open(os.getcwd()+'/01 results/'+filename,'w',encoding='utf-8',errors='ignore') as file:
+        file.write('#####################################################################\n')
+        file.write('## Really suspicious actors and their activities (high-level look) ##\n')
+        file.write('#####################################################################\n\n')
+        for client in unique_client_ip_set:
+            next_line = False
+            to_write = []
+            for record in client_ip_record[client][1:]:
+                if aq.detect_sqli(record[0]) or aq.detect_rfi(record[0]) or aq.detect_web_shell(record[0]):
+                    next_line = True
+                    to_write.append(str(record[1].strftime('%Y-%m-%d %H:%M:%S')+' '+record[0].strip(' - ')))
+            if next_line:
+                file.write('## '+client+' \'s suspicious connections ##\n')
+                for record in to_write:
+                    file.write(record+'\n')
+                file.write('\n')
+    print('done.')
+
+##cherry-on-top csv file
+    print('late addition: csv output for further analysis')
+    dump_reader.dump_csv()
+        
+##start_script
+if __name__ == '__main__':
+    if dump_reader.try_dump():
+        ddump = dump_reader.read_dump()
+        unique_client_ip_set,client_ip_record = ddump[0],ddump[1]
+        print_results(unique_client_ip_set,client_ip_record)
+    else:
+        define_variables()
+            
+##try:
+##    file = open('result/[1] IP Address List.txt','w')
+##except FileNotFoundError:
+##    file = open('result/[1] IP Address List.txt','w+')
+
+##def find_attacks(filename,keys):
+##    readfile = open (filename, "r", encoding="utf-8", errors="ignore")
+##    current_line = readfile.readline()
+##
+##    counter = 0
+##    while current_line != '':
+##        if keys[0] in current_line:
+##            print (current_line)
+##            counter+=1
+##        current_line = readfile.readline()
+##    print (counter)
+
+#find_attacks("CTF1.log",["SELECT","database"])
+
+
